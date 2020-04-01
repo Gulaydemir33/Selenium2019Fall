b1d7862726a8c299feba5723e2b2e4aa8b8e0eae
Gulaydemir33 <gulaydemir25@gmail.com>
2020-04-01 13:36:48 -0500

    CheckBoxesTest

---
+++ src/test/java/day5/TestForiFrame.java
@@ -0,0 +1,41 @@
+package day5;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import utilities.BrowserUtils;
+
+public class TestForiFrame {
+    public static void main(String[] args) {
+
+        WebDriverManager.chromedriver().version("79").setup();
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("http://practice.cybertekschool.com/iframe");
+
+        BrowserUtils.wait(4);
+
+        //it's like we are jumping to another layer
+        driver.switchTo().frame("mce_0_ifr");
+
+        WebElement textInput = driver.findElement(By.id("tinymce"));
+
+        System.out.println(textInput.getText());
+
+        BrowserUtils.wait(4);
+
+        textInput.clear(); //to delete text
+        textInput.sendKeys("Hello, World!");
+
+        //exit from the frame
+        driver.switchTo().defaultContent();
+
+        WebElement heading = driver.findElement(By.tagName("h3"));
+        System.out.println(heading.getText());
+
+        driver.quit();
+    }
+
+}
